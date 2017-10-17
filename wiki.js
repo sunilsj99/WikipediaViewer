9644739cbf1b80dd84441b917e1013e0538a04a7
Hernan Olarte <hernan940730@gmail.com>
2017-10-17 01:23:54 -0500

    Improve front-end in the search page and result page

--- wiki.js
+++ wiki.js
@@ -8,20 +8,16 @@ $(document).ready( function() {
         url:    "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + search + "&prop=info&inprop=url&utf8=&format=json",
         dataType: "jsonp",
         success: function(data){
-           if(data.query.searchinfo.totalhits !== 0)
-               {
-                   var title = [];
-                   var desc = [];
-                  for(var i = 0; i < data.continue.sroffset ; i++)
-                      {
-                          title.push(data.query.search[i].title);
-                          desc.push(data.query.search[i].snippet);
-                          var wiki = '<div class="jumbotron" id="box"><h3><a href="https://en.wikipedia.org/wiki/'+title[i]+'" target = "_blank">'+ title[i] + '</a></h3><h4>'+ desc[i] +'</h4></div>';
-                          $('#result').append(wiki);
-                          
-                      }
-                
-               }
+           if(data.query.searchinfo.totalhits !== 0) {
+               var title = [];
+               var desc = [];
+               for(var i = 0; i < data.continue.sroffset ; i++) {
+                  title.push(data.query.search[i].title);
+                  desc.push(data.query.search[i].snippet);
+                  var wiki = '<div class="jumbotron" id="box"><div class="box-title"><a href="https://en.wikipedia.org/wiki/'+title[i]+'" target = "_blank">'+ title[i] + '</a></div><div class="box-description">'+ desc[i] +'...</div></div>';
+                  $('#result').append(wiki);
+              }
+           }
         }
     });
    });
